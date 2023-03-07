4487fd91ffb6381d99c09bed1b207a3c7bba8124
AlexisBriceno98 <alex00534@hotmail.com>
2023-03-07 16:41:22 +0100

    Almost done

---
+++ script/script.js
@@ -0,0 +1,99 @@
+async function getFeaturedProducts() {
+  try {
+    const response = await fetch('https://kyh-net22.azurewebsites.net/api/products/featured');
+    const data = await response.json();
+    const productsEl = document.getElementById('product-container');
+    for (let i = 0; i < 8; i++) {
+      const product = data[i];
+      const productEl = document.createElement('div');
+      productEl.setAttribute('id', `product-${i}`);
+      productEl.classList.add('product-card');
+      const rating = (product.starRating);
+      const filledStar = '<i class="fa-solid fa-sharp fa-star"></i>';
+      const emptyStar = '<i class="fa-solid fa-regular fa-star"></i>';
+      const ratingProduct = filledStar.repeat(rating) + emptyStar.repeat(5 - rating);
+      productEl.innerHTML = `
+        <div class="product-card-image-container">
+          <img src="${product.imageUrl}" alt="${product.name}"/>
+          <div class="product-card-menu">
+            <nav class="menu-icons">
+              <a class="menu-link" href="#"><i class="fa-regular fa-code-compare"></i></a>
+              <a class="menu-link" href="#"><i class="fa-regular fa-heart"></i></a>
+              <a class="menu-link" href="#"><i class="fa-regular fa-bag-shopping"></i></a>                   
+            </nav>
+            <a href="#" id="btn-quick-view" class="btn-theme">QUICK VIEW</a>
+          </div>
+        </div>
+        <h2 class="product-card-category">${product.category}</h2>
+        <h2 class="product-card-title">${product.name}</h2>
+        <div class="product-card-rating">
+          ${ratingProduct}
+        </div>
+        <p class="product-card-price">${product.originalPrice} ${product.currency}</p>`;
+      productsEl.appendChild(productEl);
+    }
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
+}
+
+async function getNewProducts() {
+  try {
+    const response = await fetch('https://kyh-net22.azurewebsites.net/api/products/new');
+    const data = await response.json();
+    const productsEl = document.getElementById('new-products-grid');
+    for (let i = 0; i < 4; i++) {
+      const product = data[i];
+      const productEl = document.createElement('div');
+      productEl.setAttribute('id', `product-${i}`);
+      productEl.classList.add('product-card');
+      const rating1 = (product.starRating);
+      const filledStar = '<i class="fa-solid fa-sharp fa-star"></i>';
+      const emptyStar = '<i class="fa-solid fa-regular fa-star"></i>';
+      const ratingProduct = filledStar.repeat(rating1) + emptyStar.repeat(5 - rating1);
+      productEl.innerHTML = `
+        <div class="product-card-image-container">
+          <img src="${product.imageUrl}" alt="${product.name}"/>
+        </div>
+        <h2 class="product-card-category">${product.category}</h2>
+        <h2 class="product-card-title">${product.name}</h2>
+        <div class="product-card-rating">
+          ${ratingProduct}
+        </div>
+        <p class="product-card-price">${product.originalPrice} ${product.currency}</p>`;
+      productsEl.appendChild(productEl);
+    }
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
+}
+
+
+
+
+const mybutton = document.getElementById("button-scroll");
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  mybutton.style.display = "block";
+} else {
+  mybutton.style.display = "none";
+}
+
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
