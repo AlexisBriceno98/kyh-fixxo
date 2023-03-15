1eeceecfca6cfeb249be766235cdee566bbfa5db
AlexisBriceno98 <alex00534@hotmail.com>
2023-03-15 11:31:03 +0100

    Almost done with mobile view in home page

---
+++ script/validation.js
@@ -0,0 +1,72 @@
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const commentsInput = document.querySelector('#comments');
+
+const validateName = (value) => {
+    const nameError = document.querySelector('#contact-forms .contact-form-name .error');
+    if (value.length < 2)
+        nameError.style.display = 'block';
+    else
+        nameError.style.display = 'none';
+}
+
+const validateEmail = (value) => {
+    const emailError = document.querySelector('#contact-forms .contact-form-email .error');
+    const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!regEx.test(value))
+        emailError.style.display = 'block';
+    else
+        emailError.style.display = 'none';
+}
+
+const validateComment = (value) => {
+    const commentsError = document.querySelector('#contact-forms .contact-form-comments .error');
+    if (value.length < 5)
+        commentsError.style.display = 'block';
+    else
+        commentsError.style.display = 'none';
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault(); 
+
+    validateName(nameInput.value.trim());
+    validateEmail(emailInput.value.trim());
+    validateComment(commentsInput.value.trim());
+
+    const nameError = document.querySelector('#contact-forms .contact-form-name .error');
+    const emailError = document.querySelector('#contact-forms .contact-form-email .error');
+    const commentsError = document.querySelector('#contact-forms .contact-form-comments .error');
+
+    if (nameError.style.display === 'block' || emailError.style.display === 'block' || commentsError.style.display === 'block') {
+        return;
+    }
+
+    const contactForm = {
+        name: nameInput.value.trim(),
+        email: emailInput.value.trim(),
+        comments: commentsInput.value.trim()
+    };
+
+    try {
+        const res = await fetch('https://kyh-net22.azurewebsites.net/api/contacts', {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(contactForm)
+        });
+        const data = await res.json();
+      
+        console.log(data);
+      } catch{
+        
+      }
+      
+};
+
+form.addEventListener('submit', handleSubmit);
+nameInput.addEventListener('keyup', (e) => validateName(e.target.value.trim()));
+emailInput.addEventListener('keyup', (e) => validateEmail(e.target.value.trim()));
+commentsInput.addEventListener('keyup', (e) => validateComment(e.target.value.trim())); 
